ece8f5743ab403de8e54e13f76139f6d8446b3b8
Robert Kölz <rknoseman@hotmail.de>
2024-06-25 14:04:22 +0200

    fix offset for repeaters in templates

--- BiDiB-Library.Core.Test/VendorCvConverterTests.cs
+++ BiDiB-Library.Core.Test/VendorCvConverterTests.cs
@@ -12,6 +12,7 @@ namespace org.bidib.Net.Core.Test
     [TestCategory(TestCategory.UnitTest)]
     [DeploymentItem("TestData/BiDiBCV-13-120.xml", "TestData")]
     [DeploymentItem("TestData/BiDiBCV-13-104.xml", "TestData")]
+    [DeploymentItem("TestData/BiDiBCV-251-222.xml", "TestData")]
     [DeploymentItem("TestData/commonTypes.xsd", "TestData")]
     public class VendorCvConverterTests : TestClass<VendorCvConverter>
     {
@@ -26,8 +27,8 @@ namespace org.bidib.Net.Core.Test
         public void ConvertToV2Structure_ShouldResolveCvList()
         {
             // Arrange
-            string fileName = "TestData/BiDiBCV-13-120.xml";
-            VendorCv vendorCv = LoadVendorCv(fileName);
+            var fileName = "TestData/BiDiBCV-13-120.xml";
+            var vendorCv = LoadVendorCv(fileName);
 
             // Act
             Target.ConvertToV2Structure(vendorCv);
@@ -40,8 +41,8 @@ namespace org.bidib.Net.Core.Test
         public void ConvertToV2Structure_ShouldResolveTemplates()
         {
             // Arrange
-            string fileName = "TestData/BiDiBCV-13-104.xml";
-            VendorCv vendorCv = LoadVendorCv(fileName);
+            var fileName = "TestData/BiDiBCV-13-104.xml";
+            var vendorCv = LoadVendorCv(fileName);
 
             // Act
             Target.ConvertToV2Structure(vendorCv);
@@ -50,17 +51,34 @@ namespace org.bidib.Net.Core.Test
             // Assert
             vendorCv.CvDefinition[3].Nodes.Should().HaveCount(3);
         }
+        
+        [TestMethod]
+        public void ConvertToV2Structure_ShouldResolveWithNodeRepeaterOffsets()
+        {
+            // Arrange
+            var fileName = "TestData/BiDiBCV-251-222.xml";
+            var vendorCv = LoadVendorCv(fileName);
+
+            // Act
+            Target.ConvertToV2Structure(vendorCv);
+
+
+            // Assert
+            vendorCv.CvDefinition[3].Nodes.Should().HaveCount(2);
+            vendorCv.CvDefinition[3].Nodes[0].Offset.Should().Be(329);
+            vendorCv.CvDefinition[3].Nodes[0].CVs[0].Number.Should().Be("329");
+            vendorCv.CvDefinition[3].Nodes[1].Offset.Should().Be(379);
+            vendorCv.CvDefinition[3].Nodes[1].CVs[0].Number.Should().Be("379");
+        }
 
         private static VendorCv LoadVendorCv(string file)
         {
             VendorCv vendorCv = null;
             try
             {
-                XmlSerializer xmlSerializer = new XmlSerializer(typeof(VendorCv));
-                using (XmlReader reader = XmlReader.Create(file))
-                {
-                    vendorCv = xmlSerializer.Deserialize(reader) as VendorCv;
-                }
+                var xmlSerializer = new XmlSerializer(typeof(VendorCv));
+                using var reader = XmlReader.Create(file);
+                vendorCv = xmlSerializer.Deserialize(reader) as VendorCv;
             }
             catch (Exception ex)
             {
